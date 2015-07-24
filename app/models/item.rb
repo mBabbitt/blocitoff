6a984d5c33e692913996644bf9818c420bc36aa7
mBabbitt <meganbabbitt23@gmail.com>
2015-07-24 12:27:45 -0700

    added days to complete

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,7 @@ class Item < ActiveRecord::Base
   validates :user, presence: true
   validates :name, length: {minimum: 2}, presence: true
 
+def days_left
+      7 - (DateTime.now.to_date - created_at.to_date).to_i
+   end
 end
