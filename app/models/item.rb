7acf894d8fd47b381a2e5c3bf21d9c86b999481b
mBabbitt <meganbabbitt23@gmail.com>
2015-08-04 08:13:16 -0700

    fixing toggle

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ActiveRecord::Base
   belongs_to :user
 
-  default_scope { order('updated_at ASC') }
+  default_scope { order('completed ASC') }
 
   validates :user, presence: true
   validates :name, length: {minimum: 2}, presence: true
 
 def days_left
-      6 - (DateTime.now.to_date - created_at.to_date).to_i
+      7 - (DateTime.now.to_date - created_at.to_date).to_i
    end
 end
